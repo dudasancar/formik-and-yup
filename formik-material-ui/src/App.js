310c2f3fc1b0339720598e240e970389f67d2b97
Maria Carvalho <dudasancar@gmail.com>
2021-10-01 14:20:58 -0300

    ajustes artigo

--- formik-material-ui/src/App.js
+++ formik-material-ui/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import './App.css';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import ReactInputMask from 'react-input-mask';
+import { makeStyles } from '@material-ui/core';
 
 const validationSchema = Yup.object({
+  nome: Yup
+    .string('Insira seu Nome')
+    .required('Obrigatório'),
+  date: Yup
+    .string('Insira seu Nome')
+    .required('Obrigatório'),
+  gender: Yup
+    .string('Insira seu Gênero'),
   email: Yup
     .string('Insira seu email')
     .email('Email inválido')
@@ -18,14 +28,45 @@ const validationSchema = Yup.object({
     .string('Insira sua senha')
     .min(8, 'No minímo 8 caracteres')
     .required('Obrigatório'),
+  confirmPassword: Yup
+    .string('Confirme sua senha')
+    .oneOf([Yup.ref('password'), null], 'Não corresponde')
+    .required('Obrigatório'),
 });
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '25rem',
+  },
+  title: {
+    fontFamily: 'Helvetica',
+  },
+  field: {
+    marginTop: theme.spacing(2),
+  },
+  btn: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 function App() {
   const formik = useFormik({
     initialValues: {
+      nome: '',
+      date: '',
+      gender: '',
       email: '',
       cpf: '',
       password: '',
+      confirmPassword: '',
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
@@ -33,14 +74,67 @@ function App() {
     },
   });
 
+  const classes = useStyles();
   return (
-    <div className="App">
-      <form onSubmit={formik.handleSubmit}>
+    <div className={classes.container}>
+      <form onSubmit={formik.handleSubmit} className={classes.form}>
+        <h2 className={classes.title}>Cadastro de Usuário</h2>
         <TextField
           fullWidth
+          required
+          id="nome"
+          name="nome"
+          label="Nome Completo"
+          variant="outlined"
+          size="small"
+          className={classes.field}
+          InputLabelProps={{ shrink: true }}
+          value={formik.values.nome}
+          onChange={formik.handleChange}
+          error={formik.touched.nome && Boolean(formik.errors.nome)}
+          helperText={formik.touched.nome && formik.errors.nome}
+        />
+        <TextField
+          fullWidth
+          id="date"
+          name="date"
+          label="Data de Nascimento"
+          variant="outlined"
+          type="date"
+          size="small"
+          className={classes.field}
+          InputLabelProps={{ shrink: true }}
+          value={formik.values.date}
+          onChange={formik.handleChange}
+          error={formik.touched.date && Boolean(formik.errors.date)}
+          helperText={formik.touched.date && formik.errors.date}
+        />
+        <TextField
+          fullWidth
+          id="gender"
+          select
+          name="gender"
+          label="Gênero"
+          variant="outlined"
+          size="small"
+          className={classes.field}
+          InputLabelProps={{ shrink: true }}
+          value={formik.values.gender}
+          onChange={formik.handleChange}
+        >
+          <option>masculino</option>
+          <option>feminino</option>
+        </TextField>
+        <TextField
+          fullWidth
+          required
           id="email"
           name="email"
           label="Email"
+          variant="outlined"
+          size="small"
+          className={classes.field}
+          InputLabelProps={{ shrink: true }}
           value={formik.values.email}
           onChange={formik.handleChange}
           error={formik.touched.email && Boolean(formik.errors.email)}
@@ -54,27 +148,59 @@ function App() {
           onChange={formik.handleChange}
         >
           {() => <TextField 
-          fullWidth 
+          fullWidth
+          required 
           label="CPF"
           id="cpf"
           name="cpf" 
+          variant="outlined"
+          size="small"
+          className={classes.field}
+          InputLabelProps={{ shrink: true }}
           error={formik.touched.cpf && Boolean(formik.errors.cpf)}
           helperText={formik.touched.cpf && formik.errors.cpf} 
           />}
         </ReactInputMask>
         <TextField
           fullWidth
+          required
           id="password"
           name="password"
           label="Senha"
           type="password"
+          variant="outlined"
+          size="small"
+          className={classes.field}
+          InputLabelProps={{ shrink: true }}
           value={formik.values.password}
           onChange={formik.handleChange}
           error={formik.touched.password && Boolean(formik.errors.password)}
           helperText={formik.touched.password && formik.errors.password}
         />
-        <Button color="primary" variant="contained" fullWidth type="submit">
-          Entrar
+        <TextField
+          fullWidth
+          required
+          id="confirmPassword"
+          name="confirmPassword"
+          label="Confirme a senha"
+          type="password"
+          variant="outlined"
+          size="small"
+          className={classes.field}
+          InputLabelProps={{ shrink: true }}
+          value={formik.values.confirmPassword}
+          onChange={formik.handleChange}
+          error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+          helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
+        />
+        <Button 
+          color="primary" 
+          variant="contained" 
+          fullWidth 
+          type="submit"
+          className={classes.btn}
+        >
+          Cadastrar
         </Button>
       </form>
     </div>
